fe22afed011b85acdeebdc91b3b366abbb27a75d
Walrussuit101 <tjefferson@ycp.edu>
2021-12-18 14:41:30 -0500

    install two.js and add /two.js route

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,14 @@ import path from 'path';
 const server = express();
 const port = 8000;
 
-// returned the compiled client js file
+// return the compiled client js file
 server.get("/client.js", (_, res: Response) => {
 	res.sendFile(path.join(__dirname, "../dist/client.js"));
+});
+
+// return two.js library
+server.get("/two.js", (_, res: Response) => {
+	res.sendFile(path.join(__dirname, "../node_modules/two.js/build/two.min.js"));
 });
 
 // root path will send the html file
