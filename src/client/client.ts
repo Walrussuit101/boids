797a5dd9a6d57e1fd96c7619de742148172648bd
Walrussuit101 <tjefferson@ycp.edu>
2021-12-18 14:31:39 -0500

    move boids implementation to client folder
    also compile using npm script compile:client to ./dist dir

---
+++ src/client/client.ts
@@ -0,0 +1,63 @@
+import { Boid }from './models';
+import Victor from 'victor';
+import {
+	ruleOne,
+	ruleTwo,
+	ruleThree
+} from './utils';
+
+/**
+ * Initialize an array of Boids
+ *
+ * @param numBoids Number of boids to init
+ * @returns Boid[]
+ */
+const initBoids = (numBoids: number): Boid[] => {
+	const boids: Boid[] = [];
+
+	// TODO: maybe randomize their x/y positions?
+	for(let i = 0; i < numBoids; i++){
+		let boid = new Boid(i+1, new Victor(0, 0), new Victor(0, 0));
+		boids.push(boid);
+	}
+
+	return boids;
+}
+
+/**
+ * Move boids to new position after applying the 3 rules
+ *
+ * @param allBoids All current boids
+ * @returns void
+ */
+const moveBoids = (allBoids: Boid[]): void => {
+	allBoids.forEach(b => {
+
+		// apply all rules to current Boid
+		let v1 = ruleOne(b, allBoids);
+		let v2 = ruleTwo(b, allBoids);
+		let v3 = ruleThree(b, allBoids);
+
+		// set current Boid's velocity to sum of v1,2,3
+		let totalV = v1.add(v2.add(v3));
+		b.addToVelocity(totalV);
+
+		// update current Boid's position with it's new velocity
+		b.move();
+	})
+}
+
+const main = (): void => {
+	const NUM_BOIDS = 10;
+
+	const boids = initBoids(NUM_BOIDS);
+
+	console.log(boids);
+}
+
+try{
+	main();
+}catch(e){
+	console.error(e);
+	process.exit(1);
+}
