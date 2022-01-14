bc9d14d83d8aa32aa208263e250dc3cbae2c8c6f
Walrussuit101 <tjefferson@ycp.edu>
2022-01-14 10:56:56 -0500

    docs for twojsUtils

--- src/client/utils/twojsUtils.ts
+++ src/client/utils/twojsUtils.ts
@@ -1,5 +1,12 @@
 import Two, { ConstructionParams } from "twojs-ts";
 
+/**
+ * Init twojs on a HTMLElement
+ * Also add an event listener to resize twojs as the window does
+ * 
+ * @param field HTMLElement to attach twojs to
+ * @returns Two
+ */
 export const initTwojs = (field: HTMLElement): Two => {
     let fieldStyles = getComputedStyle(field);
     let twojsParams: ConstructionParams = {height: parseInt(fieldStyles.height), width: parseInt(fieldStyles.width)};
@@ -12,6 +19,12 @@ export const initTwojs = (field: HTMLElement): Two => {
     return two;
 }
 
+/**
+ * Match the twojs instance's height/width with the recomputed field's height/width
+ * 
+ * @param field HTML Element to match size of
+ * @param two Two instance
+ */
 const resize = (field: HTMLElement, two: Two) => {
     let fieldStyles = getComputedStyle(field);
     two.height = parseInt(fieldStyles.height);
